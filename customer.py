8d0753f64c20a785249555bacf33fd4ae08d6df6
gferrerni <guillermo.ferrer@protonmail.com>
2022-11-23 09:37:11 +0100

    corregidols

--- customer.py
+++ customer.py
@@ -1,14 +1,13 @@
 import requests
 import json
-def getConversionRates():
-    resp = requests.get(url="https://v6.exchangerate-api.com/v6/5047581ce1a477d56267e863/latest/EUR")
+def getConversionRates(currency):
+    resp = requests.get(url=f"https://v6.exchangerate-api.com/v6/5047581ce1a477d56267e863/latest/{currency.upper()}")
     return resp.json()
     
 def response(requestJSON):
-    ConversionRates = getConversionRates()
+    ConversionRates = getConversionRates(requestJSON["currency"])
     r = {"time": requestJSON["time"],"original_amount": requestJSON["amount"],"original_currency":requestJSON["currency"],"exchanges": []}
     for currency,exc_amount in ConversionRates["conversion_rates"].items():
-    	print(requestJSON['amount'],"*",exc_amount)
     	r["exchanges"].append({"exc_amount": requestJSON["amount"]*exc_amount,"currency": currency})
     return r
 
